8504b2b2ff0c20c8b9ee52092ab084bb8f39e84a
Vincent Lakatos <pntbllmstr74@aol.com>
2017-08-23 23:09:24 -0700

    Initial commit

---
+++ IndexAnalyzer/Classes/Constants.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexViewer
+{
+    // Im aware I can do enumerations instead, but I wanted to do it this way for categorization and neatness.
+    public static class Constants
+    {
+        public class Index
+        {
+            public static int Undefined = -1;
+        }
+        public class Logging
+        {
+            public static string FilePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Index Viewer";
+            public static string FileName = "log.txt";
+            public static string FullPath = FilePath + @"\" + FileName;
+        }
+
+        public class Configuration
+        {
+            public static string FilePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Index Viewer";
+            public static string FileName = "config.cfg";
+            public static string FullPath = FilePath + @"\" + FileName;
+        }
+
+        public class Working
+        {
+            public static string Path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Index Viewer\Temporary";
+        }
+    }
+}
---
+++ IndexAnalyzer/Objects/IndexItem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexViewer.Objects
+{
+    public class IndexItem
+    {
+        private string _Name;
+        private int _StartPosition;
+        private int _Length;
+        private string _Value;
+
+        #region Contructors
+        public IndexItem(string Name, string StartPosition, string Length)
+        {
+            _Name = Name;
+            _Value = string.Empty;
+
+            if (!int.TryParse(StartPosition, out _StartPosition))
+                _StartPosition = 0;
+
+            if (!int.TryParse(Length, out _Length))
+                _Length = 0;
+        }
+
+        public IndexItem(string Name, int StartPosition, int Length, string Value)
+        {
+            _Name = Name;
+            _Value = Value;
+            _StartPosition = StartPosition;
+            _Length = Length;
+        }
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return _Name; }
+        }
+
+        public int StartPosition
+        {
+            get { return _StartPosition; }
+        }
+
+        public int Length
+        {
+            get { return _Length; }
+        }
+
+        public string Value
+        {
+            get { return _Value; }
+            set { _Value = value; }
+        }
+        #endregion
+    }
+}
